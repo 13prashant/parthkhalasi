982e1e743312ca51429eaf3c31424cea1658ee10
Prashant Patel <pacific1392@gmail.com>
2021-03-24 21:04:35 +0530

    made navbar responsive, added playback section

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const toggleMenu = document.querySelector('.toggle')
 const navigation = document.querySelector('.navigation')
 
+const header = document.querySelector('header')
+
 toggleMenu.addEventListener('click', () => {
     if (navigation.classList.contains('active')) {
         navigation.classList.remove('active')
     } else {
         navigation.classList.add('active')
+    }
+})
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
     }
 })
